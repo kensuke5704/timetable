65c8be6bea25af045566c31c9e79241fe1bc1bdd
kensuke5704 <kensuke5704@gmail.com>
2026-06-10 21:07:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 window.TIMETABLE_CONFIG = {
   // Google Cloud Console で作成した「ウェブ アプリケーション」の OAuth クライアント ID
-  googleClientId: "",
+  googleClientId: "954896231190-qhikr89cg17tma8fcvtl095dqnr5ubfu.apps.googleusercontent.com",
 
   // スプレッドシートURL内の /d/ と /edit の間の文字列
-  spreadsheetId: "",
+  spreadsheetId: "15hs1HLgr_PI2hVYUDV2PwRajZzVVNF29P4zhi40KD_s",
 
   // 取得するシート名と範囲
-  range: "時間割!A1:K7",
+  range: "6月!A1:H107",
 
   // 画面表示
   title: "週間時間割",
-  className: "1年A組",
+  className: "FP",
 };
