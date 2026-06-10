06742fc88c090013ae9a10c6633da472d3b5ba22
kensuke5704 <kensuke5704@gmail.com>
2026-06-10 22:03:54 +0900

    Configure automatic monthly sheet range

--- config.js
+++ config.js
@@ -5,10 +5,9 @@ window.TIMETABLE_CONFIG = {
   // スプレッドシートURL内の /d/ と /edit の間の文字列
   spreadsheetId: "15hs1HLgr_PI2hVYUDV2PwRajZzVVNF29P4zhi40KD_s",
 
-  // 取得するシート名と範囲
-  range: "6月!A1:H107",
+  // 「1月」「2月」など存在する月別シートから、このセル範囲を自動取得
+  cellRange: "A1:H107",
 
   // 画面表示
-  title: "週間時間割",
   className: "FP",
 };
